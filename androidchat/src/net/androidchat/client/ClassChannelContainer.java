cccd27c5f1e24e81da25a37b61b33d8ac4b83149
TehKuja <TehKuja@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-03-25 16:57:26 +0000

    inital import

---
+++ androidchat/src/net/androidchat/client/ClassChannelContainer.java
@@ -0,0 +1,27 @@
+package net.androidchat.client;
+
+import java.util.ArrayList;
+
+public class ClassChannelContainer {
+
+    public String channame;
+    public String chantopic;
+    public ArrayList<String> whatsinchan;
+    public ArrayList<String> chanusers;
+
+    public ClassChannelContainer()
+    {
+        whatsinchan = new ArrayList<String>();
+        chanusers = new ArrayList<String>();
+    }
+
+    public void addLine(String line)
+    {
+
+        whatsinchan.add(line);
+        if (whatsinchan.size() > 30) // scrollback size
+        {
+            whatsinchan.remove(0);
+        }
+    }
+}
