db4953c1ce68d0f4c3056b74bdce1f9b09729e98
galactic.commander <galactic.commander@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-03-29 16:08:49 +0000

    Got two buttons for the "back" button and the "channels" button. "back" button actually works (I need to put a graphic or something on it) Channels button doesn't work yet. I'll start the MapChannel activity in a bit

--- trunk/AndroidChatGUI/src/com/poff/android/chatgui/ChatGUI.java
+++ trunk/AndroidChatGUI/src/com/poff/android/chatgui/ChatGUI.java
@@ -2,7 +2,6 @@ package com.poff.android.chatgui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
--- trunk/AndroidChatGUI/src/com/poff/android/chatgui/R.java
+++ trunk/AndroidChatGUI/src/com/poff/android/chatgui/R.java
@@ -15,16 +15,17 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int btn_Connect=0x7f050008;
-        public static final int btn_ShowChan=0x7f050009;
-        public static final int btn_ShowOpts=0x7f050007;
-        public static final int img_AndroidChatLogo=0x7f050006;
+        public static final int btn_Connect=0x7f050009;
+        public static final int btn_ShowChan=0x7f05000a;
+        public static final int btn_ShowOpts=0x7f050008;
+        public static final int img_AndroidChatLogo=0x7f050007;
         public static final int ircback=0x7f050000;
-        public static final int ircdisp=0x7f050003;
-        public static final int ircedit=0x7f050002;
+        public static final int ircchannel=0x7f050002;
+        public static final int ircdisp=0x7f050004;
+        public static final int ircedit=0x7f050003;
         public static final int ircsend=0x7f050001;
-        public static final int start=0x7f050004;
-        public static final int stop=0x7f050005;
+        public static final int start=0x7f050005;
+        public static final int stop=0x7f050006;
     }
     public static final class layout {
         public static final int chat=0x7f030000;
