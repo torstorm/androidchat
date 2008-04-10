928406662c40339ea0c4f4404941ab682a37b02b
galactic.commander <galactic.commander@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-04-10 07:46:57 +0000

    Map overlay and spinners working

---
+++ trunk/androidchat/src/net/androidchat/client/AndroidChatOverlay.java
@@ -0,0 +1,51 @@
+package net.androidchat.client;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.location.Address;
+import com.google.android.maps.Point;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.Overlay.PixelCalculator;
+
+import android.util.Log;
+
+
+public class AndroidChatOverlay extends Overlay {
+	private static HashMap<String, ClassChannelDescriptor>	channel_list;
+	Paint paint1 = new Paint();
+	
+	AndroidChatOverlay() {
+		channel_list = ServiceIRCService.channel_list;
+	}
+
+	public void draw(Canvas canvas, PixelCalculator pixelCalculator, boolean b) {
+		super.draw(canvas, pixelCalculator, b);
+        int[] screenCoords = new int[2];
+
+		Set<String> chanSet = channel_list.keySet();
+		ClassChannelDescriptor temp;
+
+		for(int i = 0; i < chanSet.size(); i++) {
+			Log.v("Test Chat", (String)chanSet.toArray()[i]);
+			temp = channel_list.get(chanSet.toArray()[i]);
+			StringBuffer test = new StringBuffer();
+			StringBuffer test2 = new StringBuffer();
+			test.append(temp.loc_lat);
+			test2.append(temp.loc_lng);
+			Log.v("Test lat", test.toString());
+			Log.v("Test lng", test2.toString());
+
+			 int lat = (int) temp.loc_lat * 1000000; 
+		     int lng = (int)temp.loc_lng * 1000000; 
+		     Point point = new Point(lat,lng);
+		     pixelCalculator.getPointXY(point, screenCoords);
+		     canvas.drawCircle(screenCoords[0], screenCoords[1], 9, paint1);
+		}
+
+       			
+	}
+	
+}
--- trunk/androidchat/src/net/androidchat/client/R.java
+++ trunk/androidchat/src/net/androidchat/client/R.java
@@ -18,25 +18,24 @@ public final class R {
         public static final int androidchat=0x7f040000;
         public static final int btn_Connect=0x7f04000a;
         public static final int btn_ShowChan=0x7f04000b;
-        public static final int btn_ShowOpts=0x7f04000c;
-        public static final int img_AndroidChatLogo=0x7f040009;
-        public static final int ircback=0x7f040004;
-        public static final int ircchannel=0x7f040005;
-        public static final int ircdisp=0x7f040003;
-        public static final int ircedit=0x7f040001;
-        public static final int ircscroll=0x7f040002;
-        public static final int ircsend=0x7f040006;
-        public static final int maptv=0x7f04000e;
-        public static final int mapv=0x7f04000d;
-        public static final int start=0x7f040007;
-        public static final int stop=0x7f040008;
+        public static final int btn_ShowOpts=0x7f040009;
+        public static final int chanspinner=0x7f04000e;
+        public static final int img_AndroidChatLogo=0x7f040008;
+        public static final int ircback=0x7f040001;
+        public static final int ircchannel=0x7f040002;
+        public static final int ircdisp=0x7f040005;
+        public static final int ircedit=0x7f040004;
+        public static final int ircsend=0x7f040003;
+        public static final int maptv=0x7f04000d;
+        public static final int mapv=0x7f04000c;
+        public static final int start=0x7f040006;
+        public static final int stop=0x7f040007;
     }
     public static final class layout {
         public static final int chat=0x7f030000;
-        public static final int chat_old=0x7f030001;
-        public static final int irc_control=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int map=0x7f030004;
+        public static final int irc_control=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int map=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
