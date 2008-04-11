d4e51bf5abd3682cc4fd245fac897994c63e421d
TehKuja <TehKuja@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-04-11 12:07:50 +0000

    Trivial Changes

--- androidchat/src/net/androidchat/client/R.java
+++ androidchat/src/net/androidchat/client/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int channels=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int join_channel=0x7f020002;
-        public static final int map=0x7f020003;
-        public static final int mini_icon=0x7f020004;
+        public static final int bubble=0x7f020000;
+        public static final int channels=0x7f020001;
+        public static final int dude=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int join_channel=0x7f020004;
+        public static final int map=0x7f020005;
+        public static final int mini_icon=0x7f020006;
     }
     public static final class id {
         public static final int androidchat=0x7f040000;
