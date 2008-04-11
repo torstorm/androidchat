416801353a6de205a9682194a70b3c7c59fea101
TehKuja <TehKuja@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-04-11 20:43:36 +0000

    Tweaked options so it looks good in all possible layouts.

--- trunk/androidchat/src/net/androidchat/client/R.java
+++ trunk/androidchat/src/net/androidchat/client/R.java
@@ -30,11 +30,19 @@ public final class R {
         public static final int ircedit=0x7f040002;
         public static final int ircscroll=0x7f040003;
         public static final int join_chatbut=0x7f04000c;
+        public static final int label1=0x7f040010;
+        public static final int label2=0x7f040012;
+        public static final int label3=0x7f040014;
+        public static final int label4=0x7f040016;
+        public static final int label5=0x7f040018;
         public static final int mapv=0x7f04000a;
-        public static final int optNick=0x7f04000e;
-        public static final int optPMAlert=0x7f040010;
-        public static final int optSave=0x7f04000d;
-        public static final int optSend=0x7f04000f;
+        public static final int optAutoJoin=0x7f040011;
+        public static final int optCancel=0x7f04000d;
+        public static final int optList=0x7f040013;
+        public static final int optNick=0x7f04000f;
+        public static final int optPMAlert=0x7f040017;
+        public static final int optSave=0x7f04000e;
+        public static final int optSend=0x7f040015;
         public static final int start=0x7f040005;
         public static final int stop=0x7f040006;
     }
@@ -51,15 +59,26 @@ public final class R {
         public static final int btn_connect=0x7f050001;
         public static final int btn_disconnect=0x7f050002;
         public static final int btn_options=0x7f050003;
-        public static final int irc_connected=0x7f05000a;
-        public static final int irc_connecting=0x7f050009;
-        public static final int irc_disconnected=0x7f05000c;
-        public static final int irc_error=0x7f05000b;
-        public static final int irc_started=0x7f050008;
-        public static final int irc_stopped=0x7f05000d;
+        public static final int irc_connected=0x7f05000b;
+        public static final int irc_connecting=0x7f05000a;
+        public static final int irc_disconnected=0x7f05000d;
+        public static final int irc_error=0x7f05000c;
+        public static final int irc_started=0x7f050009;
+        public static final int irc_stopped=0x7f05000e;
+        public static final int opt_cancel=0x7f050017;
+        public static final int opt_hint1=0x7f05000f;
+        public static final int opt_hint2=0x7f050010;
+        public static final int opt_hint3=0x7f050011;
+        public static final int opt_hint4=0x7f050012;
+        public static final int opt_hint5=0x7f050013;
+        public static final int opt_opt3=0x7f050014;
+        public static final int opt_opt4=0x7f050015;
+        public static final int opt_opt5=0x7f050016;
+        public static final int opt_save=0x7f050018;
         public static final int ui_getstarted=0x7f050005;
         public static final int ui_newmsg=0x7f050007;
         public static final int ui_newpm=0x7f050006;
+        public static final int ui_waiting=0x7f050008;
         public static final int welcome=0x7f050004;
     }
 }
