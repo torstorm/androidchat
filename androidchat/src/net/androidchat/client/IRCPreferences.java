4d048ba53f8add0d7376272b7aa1e151ef3673e7
michael.novakjr <michael.novakjr@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-12-24 05:38:53 +0000

    added the proper methods to the IRCPreferences class with a default value in strings.xml, also deleted R.java from the repo because its autogenerated.

--- androidchat/src/net/androidchat/client/IRCPreferences.java
+++ androidchat/src/net/androidchat/client/IRCPreferences.java
@@ -1,85 +1,38 @@
 package net.androidchat.client;
 
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 
 public class IRCPreferences extends PreferenceActivity {
-	public static final String PREFS_NAME = "androidChatPrefs";
-	private EditText nickText;
-	private EditText ajText;
 	
-	private CheckBox listBox;
-	private CheckBox locBox;
-	private CheckBox pmBox;
+	public IRCPreferences() {
+		
+	}
 
+	protected int resourceId() {
+		return R.xml.irc_preferences;
+	}
 
 	@Override
-	protected void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
+	public void onCreate(Bundle icicle) {
 		
-		setContentView(R.layout.options);
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-
-	    Button saveButton = (Button) findViewById(R.id.optSave);   
-	    saveButton.setOnClickListener(mSaveListener);
-	    
-	    Button cancButton = (Button) findViewById(R.id.optCancel);   
-	    cancButton.setOnClickListener(mCancListener);
-	    
-        nickText = (EditText) findViewById(R.id.optNick);
-        ajText = (EditText) findViewById(R.id.optAutoJoin);
-	    
-        locBox = (CheckBox) findViewById(R.id.optSend);
-	    pmBox = (CheckBox) findViewById(R.id.optPMAlert);
-	    listBox = (CheckBox) findViewById(R.id.optList);
-
-        this.setTitle("AndroidChat Options");
-        
-        listBox.setChecked(settings.getBoolean("showList", true));
-        locBox.setChecked(settings.getBoolean("sendLoc", true));
-        pmBox.setChecked(settings.getBoolean("pmAlert", true));
-        nickText.setText(settings.getString("defNick", "AndroidChat"));
-        ajText.setText(settings.getString("autoJoin", ""));
-        //settings.getBoolean("sendLoc", true);
+		super.onCreate(icicle);
+		addPreferencesFromResource(resourceId());
+		
+		Preference p = findPreference("pref_sync_wallpapers_key");
+		if (p != null) {
+			//SharedPreferences sp = p.getSharedPreferences();
+			p.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				public boolean onPreferenceChange(Preference p, Object newObjValue) {
+					Boolean newValue = (Boolean)newObjValue;
+					if (newValue == null)
+						return false;
+					p.setSummary(String.format(getResources().getString(R.string.irc_nickname), newValue));
+					return true;
+				}
+			});
+		}
 
 	} 
-	
-	private OnClickListener mCancListener = new OnClickListener() {
-		public void onClick(View v) {
-			finish();
-		}
-	};
-	
-	private OnClickListener mSaveListener = new OnClickListener() {
-		public void onClick(View v) {
-			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-			SharedPreferences.Editor editor = settings.edit(); 
-			
-			if(nickText.getText().toString().matches("[A-Za-z][0-9a-zA-Z\\[\\]\\\\^\\-_`{|}]{0,29}"))
-			{
-			editor.putBoolean("sendLoc", locBox.isChecked());
-			editor.putBoolean("pmAlert", pmBox.isChecked());
-			editor.putBoolean("showList", listBox.isChecked());
-			
-			editor.putString("defNick", nickText.getText().toString());
-			editor.putString("autoJoin", ajText.getText().toString());
-			
-			editor.commit();
-			finish();
-			} else 
-			{
-				//static 	AlertDialog 	show(Context context, CharSequence title, int iconId, CharSequence message, CharSequence buttonText, boolean cancelable)
-				//TODO: fix the alert dialog here, this is low priority right now. 
-				//AlertDialog.show(ActivityOptions.this, "Invalid Nickname", R.drawable.stop, "Your nickname contains invalid characters or is too long.", "Okay", true);
-			}
-		//	startActivity(new Intent(ActivityAndroidChatMain.this, ActivityOptions.class));
-
-		}
-	};
 }
