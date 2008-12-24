dd9d2b430ecbf214d4a31296208cb6aca8498736
michael.novakjr <michael.novakjr@2059eeb3-3c3f-0410-a2da-d7a3b85b345a>
2008-12-24 23:36:51 +0000

    removed the options button from the main screen and placed it in the menu, its funtional and the settings are properly saved.

--- androidchat/src/net/androidchat/client/ActivityAndroidChatMain.java
+++ androidchat/src/net/androidchat/client/ActivityAndroidChatMain.java
@@ -5,6 +5,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
@@ -14,6 +16,8 @@ public class ActivityAndroidChatMain extends Activity {
 
 	public Intent myConnectivtyIntent;
 
+	private static final int SETTINGS_ID = Menu.FIRST;
+	
 	/**
 	 * Called when the activity is first created.
 	 */
@@ -37,30 +41,29 @@ public class ActivityAndroidChatMain extends Activity {
 		else
 			button.setText(R.string.btn_disconnect);
 
-		Button opButton = (Button) findViewById(R.id.btn_ShowOpts);
-		if (ServiceIRCService.state <= 0)
-			opButton.setText(R.string.btn_options);
-		else
-			opButton.setText(R.string.btn_showwind);
-
-		opButton.setOnClickListener(mOptionListener);
-
 	}
 
-	private OnClickListener mOptionListener = new OnClickListener() {
-		public void onClick(View v) {
-			if (ServiceIRCService.state <= 0)
-
-				startActivity(new Intent(ActivityAndroidChatMain.this,
-						IRCPreferences.class));
-			else
-					{
-				
-					startActivity(new Intent(ActivityAndroidChatMain.this,
-							ActivityChatChannel.class));
-					}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		
+		menu.add(0, SETTINGS_ID, 0, "Settings").setIcon(android.R.drawable.ic_menu_preferences);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+			case SETTINGS_ID:
+				startActivity(new Intent(this, IRCPreferences.class));
+				break;
+			default:
+				break;
 		}
-	};
+		
+		return super.onOptionsItemSelected(item);
+	}
 
 	private OnClickListener mConnectListener = new OnClickListener() {
 		public void onClick(View v) {
